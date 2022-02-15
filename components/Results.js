c4f3cd001aca3a05a738e939d3581e738c5a3235
DevendraBAJAJ <dmbajaj.bajaj@gmail.com>
2022-02-15 09:56:54 +0530

    adjusted grid

--- components/Results.js
+++ components/Results.js
@@ -3,7 +3,7 @@ import FlipMove from "react-flip-move"
 
 function Results({ results }) {
   return (
-    <FlipMove className="px-5 my-10 grid-cols-1 sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-3 3xl:grid justify-center">
+    <FlipMove className="px-5 my-10 grid sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-3 3xl:grid justify-center">
         {results.map(result => (
             <Thumbnail key={result.id} result={result} />
         ))}
